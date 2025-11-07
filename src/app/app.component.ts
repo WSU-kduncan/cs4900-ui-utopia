3121ef7d2611fbbd693b98d67436676491c7e6b3
Makaela <ravenclaw736@gmail.com>
2025-11-07 13:45:00 -0500

    Added trainer list component to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TrainerListComponent } from './Trainer/trainer-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TrainerListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
