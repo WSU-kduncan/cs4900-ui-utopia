ba9c9d06a5abbabb37cd4aeb7878e6b33e7d97f2
crueter <crueter@eden-emu.dev>
2025-11-13 14:19:38 -0500

    client component
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Client, ClientInfoComponent } from './components/client-info/client-info.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, ClientInfoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'fitness-tracker';
+  client = new Client();
 }
