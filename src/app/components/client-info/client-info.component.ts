4fab216738147d597c16eb335062e2c147cc84ad
crueter <crueter@eden-emu.dev>
2025-11-13 14:39:05 -0500

    hw2; client detail/service
    Signed-off-by: crueter <crueter@eden-emu.dev>

--- src/app/components/client-info/client-info.component.ts
+++ src/app/components/client-info/client-info.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { FormsModule } from '@angular/forms';
+import { ClientDetailComponent } from '../client-detail/client-detail.component';
 
 export class Client {
-    name: String = "Client Name";
-    id: Number = 0;
+  name: String = 'Client Name';
+  id: Number = 0;
 }
 @Component({
   selector: 'app-client-info',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ClientDetailComponent],
   templateUrl: './client-info.component.html',
-  styleUrl: './client-info.component.scss'
+  styleUrl: './client-info.component.scss',
 })
 export class ClientInfoComponent {
-    @Input() client: Client | null = null
+  @Input() client: Client | null = null;
 
-    clients = [
-        { id: 1, name: 'James Rowe' },
-        { id: 2, name: 'John Bench' },
-        { id: 3, name: 'Jack Squat' }
-    ];
+  private clientService = inject(ClientService);
+
+  clients = this.clientService.clients;
+
+  newClientName = '';
+
+  addClient() {
+    if (this.newClientName.trim()) {
+      this.clientService.addClient(this.newClientName.trim());
+      this.newClientName = '';
+    }
+  }
 }
